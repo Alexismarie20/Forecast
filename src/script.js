cae275fe2c0ee083acab84dd860dc115473ac534
Alexismarie20 <alexismhepler@gmail.com>
2025-04-30 17:08:48 -0400

    removed emoji added a new div id,
    removed emoji added new div id, restyled to reposition element's to look good, injecting image using url based on api

--- src/script.js
+++ src/script.js
@@ -6,19 +6,21 @@ function refreshWeather(response) {
   let windSpeedElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
   
-  timeElement.innerHTML = formatDate(date);
-  cityElement.innerHTML = response.data.city;
-  descriptionElement.innerHTML = response.data.condition.description;
-  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
-  windSpeedElement.innerHTML = `${response.data.wind.speed} mph`;
-  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+   cityElement.innerHTML = response.data.city;
+   timeElement.innerHTML = formatDate(date);
+   descriptionElement.innerHTML = response.data.condition.description;
+   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+   windSpeedElement.innerHTML = `${response.data.wind.speed} mph`;
+   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+   iconElement.innerHTML= `<img src="${response.data.condition.icon_url}" class="weather-app-icon"/>`;
 }
 
 function formatDate(date) {
-  let minutes = date.getMinutes().toString().padStart(2, "0");
   let hours = date.getHours();
- let ampm = hours >= 12 ? "PM" : "AM";
+  let minutes = date.getMinutes().toString().padStart(2, "0");  
+  let ampm = hours >= 12 ? "PM" : "AM";
   hours = hours % 12 || 12;
   let days = [
     "Sunday",
@@ -31,13 +33,7 @@ function formatDate(date) {
   ];
   let day = days[date.getDay()];
   
-  if (minutes <10) {
-    minutes = `0${minutes}`;
-  }
-  if (hours <10) {
-    hours = `0${hours}`;
-  }
-  return `${day} ${hours}:${minutes} ${ampm}`;
+   return `${day} ${hours}:${minutes} ${ampm}`;
 }
 
 function searchCity(city) {
