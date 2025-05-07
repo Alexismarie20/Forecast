8bf4374354b60b7082bae9673c4aa544ea8eb5c7
Alexismarie20 <alexismhepler@gmail.com>
2025-05-06 20:09:18 -0400

    updated js

---
+++ src/script forecast.js
@@ -0,0 +1,98 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
+
+  cityElement.innerHTML = response.data.city;
+  timeElement.innerHTML = formatDate(date);
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed} mph`;
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon"/>`;
+
+  getForecast(response.data.city);
+}
+
+function formatDate(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes().toString().padStart(2, "0");
+  let ampm = hours >= 12 ? "PM" : "AM";
+  hours = hours % 12 || 12;
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  return `${days[date.getDay()]} ${hours}:${minutes} ${ampm}`;
+}
+
+function searchCity(city) {
+  let apiKey = "6o6b1f68btc323d6b261b3e92ad24092";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
+function handleSearchFormSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+
+  searchCity(searchInput.value);
+}
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  return ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"][date.getDay()];
+}
+
+function getForecast(city) {
+  let apiKey = "6o6b1f68btc323d6b261b3e92ad24092";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=imperial`;
+  axios(apiUrl).then(displayForecast);
+}
+
+function displayForecast(response) {
+  let forecastHtml = "";
+
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        ` 
+  <div class = "weather-forecast-day"> 
+      <div class = "weather-forecast-date">${formatDate(day.time)}</div>
+      
+      <img src = "${day.condition.icon_url}" class = "weather-forecast-icon"/>
+      <div class = "weather-forecast-temperatures">
+        <div class = "weather-forecast-temperature">
+          <strong>${Math.round(day.temperature.maximum)}°</strong>
+      </div>
+      <div class = "weather-forecast-temperature">${Math.round(
+        day.temperature.minimum
+      )}</div>
+    }°</div>
+    </div>
+    </div>
+    `;
+    }
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchFormSubmit);
+
+searchCity("Sarasota");
